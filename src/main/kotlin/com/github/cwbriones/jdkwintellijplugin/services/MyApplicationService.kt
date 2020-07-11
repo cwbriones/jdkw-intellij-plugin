83173f54f9c5d6fb82e133807490384bf8fb1278
GitHub Action <action@github.com>
2020-07-11 19:36:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/cwbriones/jdkwintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.cwbriones.jdkwintellijplugin.services
+
+import com.github.cwbriones.jdkwintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
